084e3f92005f8a6063e72b7dc3b6062256bf3256
axeliparrea <axeliparrea>
2024-11-04 12:25:35 -0600

    commit

---
+++ Firs-MultiAgents-Implementatio/main.py
@@ -0,0 +1,21 @@
+# Axel Eduardo Iparrea Ramos
+# Multi-Agent System Implementation with AgentPy
+# - Purpose: This project uses the AgentPy library to model a robot agent navigating a room
+#   represented as a grid of cells, collecting rewards and avoiding penalties according to specific rules.
+
+import agentpy as ap
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+
+def model(self):
+
+
+    break
+
+def agent(self):
+    self.y = model
